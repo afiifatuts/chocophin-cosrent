10f27577a34c1c455f55d5a52e5b0dac6818083c
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2022-12-22 10:45:29 +0700

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({children})=> {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Chocophinil Costrent</title>
+      </Head>
+      <header>
+        <Navbar/>
+        <main className='main-container'>
+          {children}
+        </main>
+        <footer>
+          <Footer/>
+        </footer>
+      </header>
+    </div>
+  )
+}
+
+export default Layout
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'oahce01h',
+    dataset: 'production'
+  }
+})
